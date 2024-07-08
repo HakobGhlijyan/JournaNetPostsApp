24cd09f09304c9814ff1f11e4b9e7e093e6761bf
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-07-08 14:05:45 +0400

    Initial Commit

---
+++ JournaNetPostsApp/JournaNetPostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaNetPostsAppApp.swift
+//  JournaNetPostsApp
+//
+//  Created by Hakob Ghlijyan on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct JournaNetPostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
